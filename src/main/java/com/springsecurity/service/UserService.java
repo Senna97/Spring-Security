e95fba41db2148c068d92acbb72491a1b264af38
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-28 14:15:27 +0900

    [update] UserService.java userName 중복 체크 기능

--- src/main/java/com/springsecurity/service/UserService.java
+++ src/main/java/com/springsecurity/service/UserService.java
@@ -1,13 +1,35 @@
 package com.springsecurity.service;
 
+import com.springsecurity.domain.User;
 import com.springsecurity.domain.dto.UserDto;
 import com.springsecurity.domain.dto.UserJoinRequest;
+import com.springsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
     public UserDto join(UserJoinRequest userJoinRequest) {
-        return new UserDto();
+
+        // 회원 userName(id) 중복 Check
+        // 중복이면 회원가입 X -> Exception(예외 발생)
+        userRepository.findByUserName(userJoinRequest.getUserName())
+                .ifPresent(user -> {
+                    throw new RuntimeException("입력하신 userName은 중복으로 회원가입이 불가합니다.");
+                });
+
+        // 회원가입: ,save()
+        User savedUser = userRepository.save(userJoinRequest.toEntity());
+
+
+        return UserDto.builder()
+                .id(savedUser.getId())
+                .userName(savedUser.getUserName())
+                .emailAddress(savedUser.getEmailAddress())
+                .build();
     }
 }

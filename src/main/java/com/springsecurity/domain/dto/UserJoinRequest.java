8984dc29c15c4d19c9c20b6848dcbd61f676ebbb
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-28 14:13:48 +0900

    [update] UserJoinRequest.java .toEntity() 추가

--- src/main/java/com/springsecurity/domain/dto/UserJoinRequest.java
+++ src/main/java/com/springsecurity/domain/dto/UserJoinRequest.java
@@ -1,5 +1,6 @@
 package com.springsecurity.domain.dto;
 
+import com.springsecurity.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,12 @@ public class UserJoinRequest {
     private String userName;
     private String password;
     private String emailAddress;
+
+    public User toEntity() {
+        return User.builder()
+                .userName(this.userName)
+                .password(this.password)
+                .emailAddress(this.emailAddress)
+                .build();
+    }
 }

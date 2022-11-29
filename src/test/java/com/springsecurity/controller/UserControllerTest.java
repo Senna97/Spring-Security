05e3a1160748554bf4125c6353aa26f78adc2538
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-29 13:21:44 +0900

    [test 수정]: add @WithMockUser & .with(csrf())

---
+++ src/test/java/com/springsecurity/controller/UserControllerTest.java
@@ -0,0 +1,77 @@
+package com.springsecurity.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springsecurity.domain.dto.UserDto;
+import com.springsecurity.domain.dto.UserJoinRequest;
+import com.springsecurity.exception.ErrorCode;
+import com.springsecurity.exception.SpringSecurityAppException;
+import com.springsecurity.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    @WithMockUser
+    void joinSuccess() throws Exception {
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userName("wkdtjgus0319")
+                .password("191919")
+                .emailAddress("wkdtjgus0319@naver.com")
+                .build();
+
+        when(userService.join(any())).thenReturn(mock(UserDto.class));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패")
+    @WithMockUser
+    void joinFail() throws Exception {
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userName("wkdtjgus0319")
+                .password("191919")
+                .emailAddress("wkdtjgus0319@naver.com")
+                .build();
+
+        when(userService.join(any())).thenThrow(new SpringSecurityAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+}
